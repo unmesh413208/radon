2f7ed04c12c981feda6e96aeacc86a7a4cb58272
unmesh413208 <106230792+unmesh413208@users.noreply.github.com>
2022-06-01 17:02:31 +0530

    assignment

---
+++ src/routes/logger.js
@@ -0,0 +1,11 @@
+const endpoint = "https://www.functionup.org"
+const batch = "radon"
+
+
+const log = function (){
+    console.log("i am inside the log function");
+}
+
+module.exports.endpoint = endpoint
+module.exports.batch = batch 
+module.exports.log = log
