3bef85900d2fda10c25bb8fa822ca97b0f6ee07a
unmesh413208 <106230792+unmesh413208@users.noreply.github.com>
2022-06-02 14:20:07 +0530

    new

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -3,4 +3,5 @@ const Welcome = function() {
     console.log("Welcome to my application. I am Unmesh and a part of FunctionUp radon cohort")
 }
 
+
 module.exports.Welcome = Welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
+const Name = require('../logger/logger')
+
+const Date = require('../util/helper')
+
+const Trim = require('../validator/formattor')
+
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('My latest and this first ever api!')
+    Name.Welcome()
+    Date.printDate()
+    Date.printMonth()
+    Date.getBatchInfo()
+    Trim.trim()
+    Trim.changetoLowerCase()
+    Trim.changetoUpperCase()
+
+
 });
+
+
+
 
 module.exports = router;
 // adding this comment for no reason
